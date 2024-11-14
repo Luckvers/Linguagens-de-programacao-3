675a300b8187f5a57513a7e6f95427ba411078cf
Luckvers <luccas.silva31@outlook.com>
2024-11-14 15:53:36 -0300

    Aula JavaScript - v1.4

--- JS/pessoa.js
+++ JS/pessoa.js
@@ -2,7 +2,7 @@ const pessoa = {
     nome: "Ana",
     idade: 30,
     saudacao: function() {
-        return `Olá, meu nome é $(this.nome), e tenho $(this.idade) anos.`;
+        return `Olá, meu nome é ${this.nome}, e tenho ${this.idade} anos.`;
     }
 };
 console.log(pessoa.nome);
